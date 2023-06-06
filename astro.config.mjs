20b22cdfd8ff6f7fa9d6c4fae43392cc59751596
dan-candeira <danielbarr16@gmail.com>
2023-06-05 22:08:32 -0300

    feat: add image component astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,11 @@
 import { defineConfig } from "astro/config";
-
 import preact from "@astrojs/preact";
+
+import image from "@astrojs/image";
 
 // https://astro.build/config
 export default defineConfig({
-	// docs for react to preact
-	// https://preactjs.com/guide/v10/switching-to-preact/
-	integrations: [preact()],
+  // docs for react to preact
+  // https://preactjs.com/guide/v10/switching-to-preact/
+  integrations: [preact(), image()]
 });
