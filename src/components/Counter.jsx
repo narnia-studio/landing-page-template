a2c873732aebbfe46b7c40e4c31a92209cb041df
dan-candeira <danielbarr16@gmail.com>
2023-06-06 13:11:53 -0300

    feat: add posts page

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -6,10 +6,10 @@ export default function Counter() {
 	return (
 		<>
 			<p>Counter: {value}</p>
-			<button onClick={() => setValue(value + 1)}>
+			<button type='button' onClick={() => setValue(value + 1)}>
 				Increment
 			</button>
-			<button onClick={() => setValue(value - 1)}>
+			<button type='button' onClick={() => setValue(value - 1)}>
 				Decrement
 			</button>
 			<div role='alert' className='vh' aria-live='polite'>
