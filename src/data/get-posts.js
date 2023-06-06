e8ac38b2c83a7b644dd15e0c693e9222fa419574
dan-candeira <danielbarr16@gmail.com>
2023-06-06 13:42:45 -0300

    feat: update code to fetch blog posts

--- src/data/get-posts.js
+++ src/data/get-posts.js
@@ -1,35 +1,65 @@
-const { Client } = require("@notionhq/client");
-const { NotionToMarkdown } = require("notion-to-md");
-const { marked } = require("marked");
-const slugify = require("slugify");
+// © Daniel Schulz - https://iamschulz.com/from-notion-to-eleventy/
 
-module.exports = async () => {
-	// connection + get data notion
-	const notion = new Client({ auth: import.meta.env.NOTION_TOKEN });
+import { Client } from "@notionhq/client";
+import { NotionToMarkdown } from "notion-to-md";
+import { marked } from "marked";
+import slugify from "slugify";
+
+export const getPosts = async () => {
+	// connects to notion API
+	const notion = new Client({ auth: import.meta.env.NOTION_KEY });
 	const n2m = new NotionToMarkdown({ notionClient: notion });
 
 	const databaseId = import.meta.env.NOTION_BLOG_ID;
 	const db = await notion.databases.query({
 		database_id: databaseId,
-		filter: [
-			{
-				property: "draft",
-				checkbox: {
-					equals: false,
-				},
+		filter: {
+			property: "publish",
+			checkbox: {
+				equals: true,
 			},
-			{
-				property: "publish",
-				checkbox: {
-					equals: false,
-				},
-			},
-		],
+		},
 		sorts: [
 			{
-				property: "date",
+				timestamp: "last_edited_time",
 				direction: "descending",
 			},
 		],
 	});
+
+	const getContent = async (id) => {
+		const mdblocks = await n2m.pageToMarkdown(id);
+		return n2m.toMarkdownString(mdblocks);
+	};
+
+	const posts = db.results.map((result) => {
+		return {
+			id: result.id,
+			title: result.properties["title"].title.pop().plain_text,
+			content: "",
+			cover: result.cover?.file?.url || result.cover?.external?.url,
+			coverAlt:
+				result.properties["coverAlt"]?.rich_text.pop()?.plain_text ||
+				"",
+			date: result.properties["last_edited_time"],
+			socialImage:
+				result.properties["socialImage"]?.files[0]?.file?.url ||
+				result.properties["socialImage"]?.files[0]?.external?.url,
+			summary:
+				result.properties["summary"]?.rich_text.pop()?.plain_text || "",
+			slug: "",
+		};
+	});
+
+	for (let i = 0; i < posts.length; i++) {
+		const _page = posts[i];
+		const _mdContent = await getContent(_page.id);
+		_page.content = marked(_mdContent);
+		_page.slug = slugify(_page.title, {
+			lower: true,
+			remove: /[*+~.()'"!:@]/g,
+		});
+	}
+
+	return posts;
 };
