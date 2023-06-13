4428ffcf21e31a945d1b9b65aa4295dcf221575a
dan-candeira <danielbarr16@gmail.com>
2023-06-13 00:49:57 -0300

    feat: add animation control component to play/pause animation

---
+++ src/components/AnimationControl.jsx
@@ -0,0 +1,96 @@
+import { useState, useEffect, useRef } from "preact/hooks";
+
+export default function AnimationControl() {
+	const [enableMotion, setEnableMotion] = useState(false);
+	const [prefersMotion, setPrefersMotion] = useState(null);
+	const [enhance, setEnhance] = useState(false);
+
+	const motionStorageKey = "enable-astro-blog-motion";
+
+	const updateClass = (value) => {
+		if (value) {
+			document.documentElement.classList.add("animate")
+		} else {
+			document.documentElement.classList.remove("animate")
+		}
+	}
+
+	const handleAnimationState = (value) => {
+		const _enabled = !value;
+		setEnableMotion(_enabled);
+		localStorage.setItem(motionStorageKey, String(_enabled));
+		updateClass(_enabled);
+	};
+
+	useEffect(() => {
+		try {
+			// checks document has class "enhanced" 
+			// which indicates presence of javascript
+			const _enhanced =
+				document.documentElement.classList.contains("enhanced");
+			setEnhance(_enhanced);
+
+			setPrefersMotion(
+				window.matchMedia("(prefers-reduced-motion: no-preference)")
+			);
+
+			// handle local storage
+			const _storedValue = localStorage.getItem(motionStorageKey);
+			const _motionEnabled =
+				_storedValue != undefined ? _storedValue == "true" : undefined;
+
+
+			if (_motionEnabled != undefined) {
+				setEnableMotion(_motionEnabled);
+				updateClass(_motionEnabled);
+			} else {
+				const _enabled = prefersMotion.matches;
+				setEnableMotion(_enabled);
+				updateClass(_enabled);
+				localStorage.setItem(motionStorageKey, String(_enabled));
+			}
+		} catch (error) {}
+	}, []);
+
+	return (
+		<button
+			data-visible={enhance}
+			className="animation-control"
+			type="button"
+			role="switch"
+			aria-pressed={enableMotion}
+			id="playButton"
+			onClick={() => handleAnimationState(enableMotion)}
+		>
+			<span className="vh">
+				Animation is {enableMotion ? "enabled" : "disabled"}
+			</span>
+			<svg
+				id="pause"
+				xmlns="http://www.w3.org/2000/svg"
+				fill="none"
+				viewBox="0 0 224 352"
+				aria-hidden="true"
+				focusable="false"
+			>
+				<path
+					d="M80 352H0V0h80v352Zm144 0h-80V0h80v352Z"
+					fill="currentColor"
+				/>
+			</svg>
+			<svg
+				id="play"
+				xmlns="http://www.w3.org/2000/svg"
+				fill="none"
+				viewBox="0 0 320 384"
+				aria-hidden="true"
+				focusable="false"
+			>
+				<path
+					d="M272.53 0v163.52L0 0v384l272.53-163.52V384H320V0h-47.47Z"
+					fill="currentColor"
+				/>
+			</svg>
+		</button>
+	);
+}
--- src/components/Marquee.jsx
+++ src/components/Marquee.jsx
@@ -1,99 +1,18 @@
-import { useState, useEffect, useRef } from "preact/hooks";
-
 export default function Marquee(props) {
-	const [enableMotion, setEnableMotion] = useState(false);
-	const [prefersMotion, setPrefersMotion] = useState(null);
-	const [enhance, setEnhance] = useState(false);
-	const button = useRef(null);
-
-	const motionStorageKey = "enable-astro-blog-motion";
-
-	const handleAnimationState = (value) => {
-		setEnableMotion(!value);
-		localStorage.setItem(motionStorageKey, String(!value));
-	};
-
-	useEffect(() => {
-		try {
-			// only display button if js is available
-			// button.removeAttribute('hidden');
-			// checks if js is enabled
-			const _enhanced =
-				document.documentElement.classList.contains("enhanced");
-			setEnhance(_enhanced);
-
-			setPrefersMotion(
-				window.matchMedia("(prefers-reduced-motion: no-preference)")
-			);
-			const _storedValue = localStorage.getItem(motionStorageKey);
-			const _motionEnabled =
-				_storedValue != undefined ? _storedValue == "true" : undefined;
-
-			if (_motionEnabled != undefined) {
-				setEnableMotion(_motionEnabled);
-			} else {
-				setEnableMotion(prefersMotion.matches);
-				localStorage.setItem(motionStorageKey, String(enableMotion));
-			}
-		} catch (error) {}
-	}, []);
-
 	return (
-		<>
+		props.children && (
 			<div class="marquee">
-				<div class="wrapper">
-					<button
-						data-visible={enhance}
-						type="button"
-						role="switch"
-						aria-pressed={enableMotion}
-						id="playButton"
-						onClick={() => handleAnimationState(enableMotion)}
+				{Array.from({ length: 4 }, (_, i) => (
+					<ul
+						className="marquee__content"
+						role="list"
+						aria-hidden={i > 0 || undefined}
+						key={i}
 					>
-						<span className="vh">
-							Animation is {enableMotion ? "enabled" : "disabled"}
-						</span>
-						<svg
-							id="pause"
-							xmlns="http://www.w3.org/2000/svg"
-							fill="none"
-							viewBox="0 0 224 352"
-							aria-hidden="true"
-							focusable="false"
-						>
-							<path
-								d="M80 352H0V0h80v352Zm144 0h-80V0h80v352Z"
-								fill="currentColor"
-							/>
-						</svg>
-						<svg
-							id="play"
-							xmlns="http://www.w3.org/2000/svg"
-							fill="none"
-							viewBox="0 0 320 384"
-							aria-hidden="true"
-							focusable="false"
-						>
-							<path
-								d="M272.53 0v163.52L0 0v384l272.53-163.52V384H320V0h-47.47Z"
-								fill="currentColor"
-							/>
-						</svg>
-					</button>
-					<div className="marquee__content">
-						{props.items &&
-							Array.from({ length: 4 }, (_, i) => (
-								<ul
-									role="list"
-									aria-hidden={i > 0 || undefined}
-									key={i}
-								>
-									{props.children}
-								</ul>
-							))}
-					</div>
-				</div>
+						{props.children}
+					</ul>
+				))}
 			</div>
-		</>
+		)
 	);
 }
